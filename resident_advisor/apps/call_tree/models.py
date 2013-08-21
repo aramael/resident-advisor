846adeaee0acd62f95fba2a269d13feba4055d9d
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-08-21 17:51:56 -0400

    Added RACallProfile as 1:1 with User

--- resident_advisor/apps/call_tree/models.py
+++ resident_advisor/apps/call_tree/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class RACallProfile(models.Model):
+    user = models.OneToOneField(User)
+    phone_number = models.CharField(max_length=25)
