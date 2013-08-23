3c5fc50167d4936772cf3cd69d2bfa3cbc4611ce
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-08-23 00:29:31 -0400

    Changed Form to Remove User / Owner and add user first and last names

--- resident_advisor/apps/call_tree/forms.py
+++ resident_advisor/apps/call_tree/forms.py
@@ -1,14 +1,18 @@
 from django import forms
-from resident_advisor.libs.forms import ActionMethodForm
+from resident_advisor.libs.forms import ActionMethodForm, HideOwnerForm
 from .models import RACallProfile
 
 
-class RACallProfileForm(ActionMethodForm, forms.ModelForm):
+class RACallProfileForm(ActionMethodForm, HideOwnerForm, forms.ModelForm):
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
 
     POSSIBLE_ACTIONS = {'_save', }
 
     class Meta:
         model = RACallProfile
+        exclude = ['formatted_phone_number', ]
 
     def location_redirect(self, action, instance):
         if action == '_save':
